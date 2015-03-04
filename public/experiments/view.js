2d49b71a4d34cc9e0ea48dfd999c0a3e89eb135b
Alexander Mueller <alexan.chr.mueller@gmail.com>
2015-03-04 13:34:57 +0100

    Rounded numbers

--- public/experiments/view.js
+++ public/experiments/view.js
@@ -13,7 +13,19 @@ angular.module('myApp.experimentsTable', ['ngRoute', 'datatables', 'ngResource']
         var vm = this;
 
         $resource('/api/experiments').query().$promise.then(function(experiments) {
-            vm.experiments = experiments;
+
+            var processedexp = experiments.map(function(result){
+                result.macro_result.fmeasure = Math.round(result.macro_result.fmeasure*1000)/1000
+                result.macro_result.precision =  Math.round(result.macro_result.precision*1000)/1000
+                result.macro_result.recall =  Math.round(result.macro_result.recall*1000)/1000
+                result.micro_result.fmeasure = Math.round(result.micro_result.fmeasure*1000)/1000
+                result.micro_result.precision =  Math.round(result.micro_result.precision*1000)/1000
+                result.micro_result.recall =  Math.round(result.micro_result.recall*1000)/1000
+
+                return result
+            });
+
+            vm.experiments = processedexp;
         });
 
     });
